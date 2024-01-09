2c2e2a449dba54cb84cd878857e4da51b8f4c930
Alex Weinle <alex.weinle@gmail.com>
2024-01-09 14:03:30 +0000

    Initial version objects.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Legalesign.js ES Module
+
+## Installation
+
+Use `npm` to install the Legalesign.js module:
+
+```sh
+npm install @legalesign/legalesign-js
+```
---
+++ types/api/document.d.ts
@@ -0,0 +1,62 @@
+import {Group} from "./group";
+
+/**
+ * The Document object.
+ */
+export interface Document {
+  /**
+   * Unique identifier for the document.
+   */
+  id: string;
+
+  /**
+   * String representing the object's type. Objects of the same type share the same value.
+   */
+  object: 'document';
+
+   /**
+   * The title of a document that has been sent to one or more signers.
+   */
+   name: String | null
+
+   /**
+   * A short lease url for downloading or streaming the document.
+   */
+  	link: String | null
+
+   /**
+   * City/District/Suburb/Town/Village.
+   */
+  	status: String
+
+   /**
+   * The group object associated (currently) with this document.
+   */
+  	group: Group
+
+   /**
+   * The whole template object that the document was made from.
+   */
+    template: Template
+
+	# The ID of template that the document was made from (this is for more efficient mass document queries).
+	sourceTemplateId: ID
+	# The ISO 8601 date format of the time that this resource was created.
+	created: AWSDateTime
+	# The ISO 8601 date format of the time that this resource was modified.
+	modified: AWSDateTime
+	# The participants that received this document (if any)
+	recipients: [Recipient]
+	# The audit log for this document (if any)
+	auditLog: [AuditLogItem]
+	# The name of the sender
+	senderName: String
+	# The email of the sender
+	senderEmail: AWSEmail
+	# The unique identifier of the process that created this document
+	taskId: ID
+}
+
+export namespace Document {
+  export type AvailablePayoutMethod = 'instant' | 'standard';
+}
